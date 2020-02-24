f19447a114e46ce907d7f695e824fee3bb63d17f
Chris Roeder <chris.roeder@ucdenver.edu>
2020-02-24 15:16:10 -0700

    tweak variable names in sed commands in build.sh, finishing testing up to ATLAS coming up

--- build.sh
+++ build.sh
@@ -3,16 +3,20 @@
 
 # This script setups up the gamut of OMOP schemata, runs DDL and loads data. For now,
 # limited to PostgreSQL.
-# Does it assume R, Python, or any packages???
-# (perhaps obviously) assumes BASH is available
-# It assumes PostgreSQL, and Tomcat have been installed and configured. 
+#
+# To run: 
+#   download vocabulary from ATHENA
+#   install postgresql
+#   clone git@github.com:chrisroederucdenver/omop_distro.git
+#   edit locations in variables below: GIT_BASE, DEPLOY_BASE, DB_NAME, ATHENA_VOCAB
+#   run build.sh
+# It will bring up an ATLAS web page.
+#
+# ASSUMPTIONS:
+# (perhaps obviously) BASH is available
+# R is installed
+# It assumes PostgreSQL has been installed and configured. 
 #   and assumes the  PG environment variables are set to working defaults.
-# It assumes Vocabulary has been selected, downloaded and placed ______________________
-# It assumes CDM, Achilles, WebAPI, ATLAS and vocabulary are in parallel directories cloned 
-#   from git or downloaded (vocabulary) as appropriate. And that that directory is in the 
-#   variable GIT_BASE below.
-# It assumes WebAPI has been configured with a profile whose name corresponds to the database name
-# It and subsidiary scripts assume the following schema names: cdm, vocabulary, results, webapi
 # It doesn't do much for error checking and so assumes the user can identify and resolve issues.
 # 
 #DONE! TODO: collect the scripts into a repo
@@ -44,23 +48,20 @@ CDM_SCHEMA=cdm # all that works for the moment is cdm
 RESULTS_SCHEMA=results_x
 WEBAPI_SCHEMA=webapi_x
 SYNTHEA_SCHEMA=synthea_x
-SYNTHEA_OUTPUT=$DEPLOY_BASE/synthea_output
+SYNTHEA_OUTPUT=$GIT_BASE/synthea/output/csv
 
-ATHENA_VOCAB=$GIT_BASE/OHDSI_Vocabulary_local
+ATHENA_VOCAB=/Users/christopherroeder/work/git/misc_external/OHDSI_Vocabulary_local
 
 TOMCAT_ARCHIVE_URL="https://downloads.apache.org/tomcat/tomcat-9/v9.0.31/bin/apache-tomcat-9.0.31.tar.gz"
 TOMCAT_DIR=apache-tomcat-9.0.31
 TOMCAT_ARCHIVE="apache-tomcat-9.0.31.tar.gz"
 TOMCAT_HOME=$DEPLOY_BASE/tomcat/$TOMCAT_DIR
-TOMCAT_PORT=8081
+TOMCAT_PORT=8080
 TOMCAT_URL=http://127.0.0.1:$TOMCAT_PORT/
 
 POSTGRESQL_PORT=5432
 
 
-mkdir $GIT_BASE
-mkdir $DEPLOY_BASE
-cd $GIT_BASE
 
 
 function message  {
@@ -77,16 +78,23 @@ function message  {
     fi
 }
 
-echo "** PostgreSQL Roles"
-cat $OMOP_DISTRO/setup_postgres_roles.sql | psql 
 
-function db_prep {
-    # Drop Database, create new
-    echo "** DATABASE, $DB_NAME"
-
-    # for dev
+function shutdown_and_delete_old {
+    $TOMCAT_HOME/bin/shutdown.sh
     dropdb $DB_NAME
+    rm -rf $GIT_BASE
+    rm -rf $DEPLOY_BASE
+}
 
+function make_new {
+    echo "** PostgreSQL Roles"
+    cat $OMOP_DISTRO/setup_postgres_roles.sql | psql 
+
+    mkdir $GIT_BASE
+    mkdir $DEPLOY_BASE
+    cd $GIT_BASE
+
+    echo "** PostgreSQL DB"
     cat $OMOP_DISTRO/setup_db.sql  \
       | sed  s/XXX/$DB_NAME/g  \
       | psql  -U $UBER_DB_USER
@@ -100,36 +108,48 @@ function get_git_repos {
     # AchillesWeb has a v1.0.0 tag from 2014, changes from 2017, used current
 
     # should use tags here: TODO
-    # CommonDataModel tags are last from 2018-10-11, v6.0.0
+    # CommonDataModel tags are last from 2018-10-11, v5.3.1
     # WebAPI has tag v2.7.6 from 2020-01-22, still used current
     # Atlas has v.2.7.6 from 2020-01-23, used current
     
-    mkdir  "$GIT_BASE"
     cd $GIT_BASE
-    OHDSI_BASE="https://github.com/OHDSI"
-    repo=(
-      "CommonDataModel.git"
-      "Achilles.git"  
-      "WebAPI.git"
-      "AchillesWeb.git"
-      "Atlas.git" )
 
-    for rep in ${repo[*]}; do
-        dirname=$(echo $rep | sed s/.git//)
-        echo $rep $dirname
-        if [ ! -e $dirname ]; then
-            git clone $OHDSI_BASE/$rep
-        else
-            echo "..already have $rep"
-        fi
-    done
+    if [ ! -e Achilles ]; then
+        #svn export https://github.com/OHDSI/Achilles/trunk/
+        #mv trunk Achilles
+        git clone --depth 1 https://github.com/OHDSI/Achilles
+    fi
+
+    if [ ! -e CommonDataModel ]; then
+        svn export https://github.com/OHDSI/CommonDataModel/tags/v5.3.1
+        mv v5.3.1 CommonDataModel
+    fi
+
+    if [ ! -e WebAPI ]; then
+        svn export https://github.com/OHDSI/WebAPI/tags/v2.7.6
+        mv v2.7.6 WebAPI
+    fi
+
+    if [ ! -e ATLAS ]; then
+        svn export https://github.com/OHDSI/Atlas/tags/v2.7.6
+        mv v2.7.6 Atlas
+    fi
 
     if [ ! -e synthea ]; then
-        git clone "https://github.com/synthetichealth/synthea.git"
+        #svn export https://github.com/synthetichealth/synthea/tag/v2.5.0
+        #mv v2.5.0 synthea
+        svn export https://github.com/synthetichealth/synthea/trunk
+        mv trunk synthea
     fi
 
     if [ ! -e ETL-Synthea ]; then
-        git clone https://github.com/chrisroederucdenver/ETL-Synthea.git
+        svn export https://github.com/chrisroederucdenver/ETL-Synthea/tags/v0.9.3cr
+        mv v0.9.3cr ETL-Synthea
+    fi
+
+    if [ ! -e AchillesWeb ]; then
+        svn export https://github.com/OHDSI/AchillesWeb/trunk
+        mv trunk AchillesWeb
     fi
 }
 
@@ -138,12 +158,12 @@ function vocabulary {
 # This ddl is adapted from CommonDataModel
     echo "** VOCABULARY"
     cat $OMOP_DISTRO/setup_schema.sql \
-      | sed  s/$VOCABULAR_SCHEMA/$DB_NAME/g  \
+      | sed  s/XXX/$VOCABULARY_SCHEMA/g  \
       | psql  -U ohdsi_admin_user  $DB_NAME
 # vocabulary*.ddl needs schema templating
-    cat $OMOP_DISTOR/ddl/vocabulary.ddl | psql -U ohdsi_admin_user $DB_NAME
+    cat $OMOP_DISTRO/ddl/vocabulary.ddl | psql -U ohdsi_admin_user $DB_NAME
 
-    $ this is an unzipped package of vocabulary from athena
+    # this is an unzipped package of vocabulary from athena
     cd $ATHENA_VOCAB
     cat load_copy.sql | psql -U ohdsi_admin_user $DB_NAME
     cat $OMOP_DISTRO/ddl/vocabulary_indexes.ddl | psql -U ohdsi_admin_user $DB_NAME
@@ -154,7 +174,7 @@ function vocabulary {
 
 function cdm {
 # This ddl is adapted from CommonDataModel
-    echo "** CDM"
+    echo "** CDM $DB_NAME"
     cat $OMOP_DISTRO/setup_schema.sql \
       | sed  s/XXX/$CDM_SCHEMA/g  \
       | psql  -U ohdsi_admin_user  $DB_NAME
@@ -171,7 +191,6 @@ function synthea {
     else
         echo "...using previous run of synthea"
     fi
-    SYNTHEA_OUTPUT=$GIT_BASE/synthea/output/csv  # unused: TODO: use it, pass the location into R as a parameter
     cd $GIT_BASE
 }
 
@@ -193,8 +212,9 @@ function synthea_etl {
     sed -i .old  s/SYNTHEA/$SYNTHEA_SCHEMA/ local_load.R
     sed -i .old  s/CDM/$CDM_SCHEMA/ local_load.R
     sed -i .old  s/VOCABULARY/$VOCABULARY_SCHEMA/ local_load.R
-    sed -i .old  s/OUTPUT/$SYNTHEA_OUTPUT/ local_load.R
+    sed -i .old  "s|OUTPUT|$SYNTHEA_OUTPUT|" local_load.R
     mkdir $SYNTHEA_OUTPUT
+
     Rscript local_load.R
     message $? " synthea etl failed" 5
     cd $GIT_BASE
@@ -212,16 +232,15 @@ function synthea_etl {
 function results_schema {
     # requires WebAPI and Tomcat to be up and running
     echo "** RESULTS SCHEMA"
-    cat $GIT_BASE/WebAPI/setup_results_schema.sql \
+    cat $OMOP_DISTRO/setup_schema.sql \
       | sed  s/XXX/$RESULTS_SCHEMA/g  \
       | psql  -U ohdsi_admin_user  $DB_NAME
 }
 
-function results {
-    # requires WebAPI and Tomcat to be up and running, is rumored to be run as part of 
-    # running achilles: 
+function get_results_ddl {
+    # Achilles sets this up based on its config.
+    # Here as a way of debugging.
     # https://forums.ohdsi.org/t/ddl-scripts-for-results-achilles-results-derived-etc/9618/6
-    ##cat $GIT_BASE/Achilles/results.ddl 
     wget -o - http://127.0.0.1:$TOMCAT_PORT/WebAPI/ddl/results?dialect=postgresql \
       | sed  s/results/$RESULTS_SCHEMA/g  \
       | psql -U ohdsi_admin_user $DB_NAME
@@ -238,7 +257,8 @@ function achilles {
     sed -i .old s/CDM_SCHEMA/$CDM_SCHEMA/ run_achilles.R
     sed -i .old s/VOCABULARY_SCHEMA/$VOCABULARY_SCHEMA/ run_achilles.R
     sed -i .old s/RESULTS_SCHEMA/$RESULTS_SCHEMA/ run_achilles.R
-    Rscript $GIT_BASE/Achilles/run_achilles_synthea_omop.R
+  cat run_achilles.R . 
+    Rscript run_achilles.R . 
     message $? " achilles failed"  6
 } 
 
@@ -257,19 +277,23 @@ function build_webapi {
 
     ## build WebAPI with profile using dbname
     cd $GIT_BASE/WebAPI
-    sed -i .old1 s/PORT/$POSTGRESQL_PORT/     $OMOP_DISTRO/webapi_settings.xml
-    sed -i .old  s/SCHEMA/$WEBAPI_SCHEMA/ $OMOP_DISTRO/webapi_settings.xml
-    sed -i .old  s/DB_NAME/$DB_NAME/      $OMOP_DISTRO/webapi_settings.xml
-    cp $OMOP_DISTRO/webapi_settings.xml $GIT_BASE/WebAPI/WebAPIConfig/settings.xml
+    cp $OMOP_DISTRO/webapi_settings.xml .
+    sed -i .old1 s/POSTGRESQL_PORT/$POSTGRESQL_PORT/     webapi_settings.xml
+    sed -i .old  s/WEBAPI_SCHEMA/$WEBAPI_SCHEMA/ webapi_settings.xml
+    sed -i .old  s/DB_NAME/$DB_NAME/      webapi_settings.xml
+    mkdir $GIT_BASE/WebAPI/WebAPIConfig
+    cp webapi_settings.xml $GIT_BASE/WebAPI/WebAPIConfig/settings.xml
     mvn clean package -P $DB_NAME -D skipTests -s $GIT_BASE/WebAPI/WebAPIConfig/settings.xml
     message $? " WebAPI build failed" 1
 }
 
 function install_postgres {
-    echo "install_postgres? LOL HAHAHAHAHAH" 
+    echo "install_postgres? ...not yet" 
 }
 
 function install_tomcat {
+    echo "** TOMCAT"
+    kill $(ps -aef | grep tomcat | awk '{print $2}')
     cd $DEPLOY_BASE
     mkdir tomcat
     cd tomcat
@@ -283,14 +307,22 @@ function install_tomcat {
 
     cd $TOMCAT_HOME
     sed -i .old s/8080/$TOMCAT_PORT/g conf/server.xml
+    cat conf/tomcat-users.xml      | awk 'NR==44 {print " <role rolename=\"manager-gui\"/>  " } {print}' > conf/tomcat-users.xml.new1
+    cat conf/tomcat-users.xml.new1 | awk 'NR==45 {print " <role rolename=\"tomcat\"/>  " } {print}' > conf/tomcat-users.xml.new4
+    cat conf/tomcat-users.xml.new4 | awk 'NR==46 {print "<user username=\"tomcat\" password=\"Harmonization\" roles=\"tomcat,manager-gui\"/>" } {print}' > conf/tomcat-users.xml.new
+    mv conf/tomcat-users.xml conf/tomcat-users.xml.old
+    mv conf/tomcat-users.xml.new conf/tomcat-users.xml
+    rm conf/tomcat-users.xml.new1
+    rm conf/tomcat-users.xml.new4
+
     # for testing...
     netstat -na | grep 8005
-    if [[ "$status" == "0" ]] ; then 
-        # there's another server here already, move out of the way
-        echo "existing server detected, deploying this one to tomcat ports 8006 and 8010"
-        sed -i .old s/8005/8006/g conf/server.xml
-        sed -i .old s/8009/8010/g conf/server.xml
-    fi
+###    if [[ "$status" == "0" ]] ; then 
+###        # there's another server here already, move out of the way
+###        echo "existing server detected, deploying this one to tomcat ports 8006 and 8010"
+###        sed -i .old s/8005/8006/g conf/server.xml
+###        sed -i .old s/8009/8010/g conf/server.xml
+###    fi
     bin/startup.sh
     sleep 10
     wget http://127.0.0.1:$TOMCAT_PORT
@@ -309,39 +341,60 @@ function install_webapi {
     cp $WARFILE $TOMCAT_HOME/webapps
     message $? " copying war to Tomcat failed" 2
     # TODO: grep the log for action and success?
-    open $TOMCAT_URL/WebAPI
 }
 
 function insert_source_rows {
-    echo "insert into webapi.source (source_id, source_name, source_key, source_connection, source_dialect, username, password) values (99, 'Synthea in OMOP', 'synthea_omop', 'jdbc:'jdbc:postgresql://localhost:$POSTGRESQL_PORT/$DB_NAME, 'postgresql', 'ohsi_app_user', '');" | psql -U ohdsi_admin_user $DB_NAME
+    echo "delete from $WEBAPI_SCHEMA.source_daimon;" | psql $DB_NAME
+
+    echo "insert into $WEBAPI_SCHEMA.source (source_id, source_name, source_key, source_connection, source_dialect, username, password) values (99, 'Synthea in OMOP', '$DB_NAME', 'jdbc:postgresql://localhost:$POSTGRESQL_PORT/$DB_NAME', 'postgresql', 'ohdsi_app_user', '');" | psql -U ohdsi_admin_user $DB_NAME
     echo "" 
-    echo "insert into webapi.source_daimon (source_id, daimon_type, table_qualifier, priority) values (99, 0, 'cdm', 1);" | psql -U ohdsi_admin_user $DB_NAME
-    echo "insert into webapi.source_daimon (source_id, daimon_type, table_qualifier, priority) values (99, 1, 'vocabulary', 1);" | psql -U ohdsi_admin_user $DB_NAME
-    echo "insert into webapi.source_daimon (source_id, daimon_type, table_qualifier, priority) values (99, 2, 'results', 1);" | psql -U ohdsi_admin_user $DB_NAME
+    echo "insert into $WEBAPI_SCHEMA.source_daimon (source_id, daimon_type, table_qualifier, priority) values (99, 0, '$CDM_SCHEMA', 1);" | psql -U ohdsi_admin_user $DB_NAME
+    echo "insert into $WEBAPI_SCHEMA.source_daimon (source_id, daimon_type, table_qualifier, priority) values (99, 1, '$VOCABULARY_SCHEMA', 1);" | psql -U ohdsi_admin_user $DB_NAME
+    echo "insert into $WEBAPI_SCHEMA.source_daimon (source_id, daimon_type, table_qualifier, priority) values (99, 2, '$RESULTS_SCHEMA', 1);" | psql -U ohdsi_admin_user $DB_NAME
+    
+    echo "select * from $WEBAPI_SCHEMA.source_daimon;" | psql $DB_NAME
+    echo "select * from $WEBAPI_SCHEMA.source;" | psql $DB_NAME
 }
 
 function atlas {
     echo "**ATLAS"
     cp -r $GIT_BASE/Atlas $TOMCAT_HOME/webapps
-    sed -i .old1  s/NAME/Atlas/ $OMOP_DISTRO/config-local.js 
-    sed -i .old   s/PORT/$TOMCAT_PORT/ $OMOP_DISTRO/config-local.js 
     cp $OMOP_DISTRO/config-local.js $TOMCAT_HOME/webapps/Atlas/js/
-    open $TOMCAT_URL/WebAPI/Atlas
+    sed -i .old1  s/APP_NAME/WebAPI/            $TOMCAT_HOME/webapps/Atlas/js/config-local.js
+    sed -i .old2  s/TOMCAT_PORT/$TOMCAT_PORT/  $TOMCAT_HOME/webapps/Atlas/js/config-local.js
+    open $TOMCAT_URL/Atlas/#/home
 }
 
-install_postgres
+function test_webapi_sources {
+    wget http://127.0.0.1:$TOMCAT_PORT/WebAPI/source/sources
+    CONTENTS=$(cat sources)
+    rm sources
+    if [[ $CONTENTS == '[]' ]] ;then
+        echo "ERROR, no data sources: \"$CONTENTS\" $WEBAPI_SCHEMA $DB_NAME"
+        # this means WebAPI isn't reading the source and source_daimon tables it should be.
+    else
+        echo "cool! data sources: \"$CONTENTS\" "
+    fi
+}
+
+##install_postgres
+shutdown_and_delete_old
+make_new
 get_git_repos 
-db_prep
 vocabulary
 cdm
 synthea
 synthea_etl
 results_schema
-    #results
+##get_results_ddl
 achilles
 install_tomcat
-###achilles_web
+######achilles_web
 build_webapi
 install_webapi
+sleep 30
 insert_source_rows
+test_webapi_sources
 atlas
+
+
--- config-local.js
+++ config-local.js
@@ -2,8 +2,8 @@ define([], function () {
 	var configLocal = {};
 
 	configLocal.api = {
-		name: 'NAME',
-		url: 'http://127.0.0.1:PORT/WebAPI/'
+		name: 'APP_NAME',
+		url: 'http://127.0.0.1:TOMCAT_PORT/APP_NAME/'
 	};
 
 	return configLocal;
