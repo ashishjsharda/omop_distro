3fc02279bbff54378fc40f9b32a74234d8d21562
Christophe Roeder <119077+croeder@users.noreply.github.com>
2020-05-07 16:23:21 -0600

    added note about Achilles 2.6 when it fails with bootstrap

--- run_achilles.R
+++ run_achilles.R
@@ -3,6 +3,9 @@ if (!require("devtools")) install.packages("devtools")
 
 if (!require("Achilles")) devtools::install_github("OHDSI/Achilles")
 # I had better luck installing from a download. TODO
+# note here that 2.6 is the way to go, not root from the git tree
+# https://forums.ohdsi.org/t/atlas-setup-failing/5858/2
+# this is about it not finding bootstrap
 
 library(Achilles)
 connectionDetails <- createConnectionDetails(
